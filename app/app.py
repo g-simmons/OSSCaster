5749e1e9853be946fe4c6d490bc76b1408a9c8d3
Gabriel Simmons <simmons.gabe@gmail.com>
2021-11-06 19:47:30 -0700

    line graph based on CSV upload

--- app/app.py
+++ app/app.py
@@ -4,12 +4,14 @@ import io
 from json import load
 
 import dash
+from dash.dash_table.DataTable import DataTable
 from dash.dependencies import Input, Output, State
 from dash import dcc
 import dash_bootstrap_components as dbc
 from dash import html
 from dash import dash_table
 from keras.models import load_model
+import plotly.graph_objs as go
 
 import pandas as pd
 
@@ -17,6 +19,8 @@ import pandas as pd
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+CSV_COLUMNS_AS_MONTHS = True
 
 # MODEL_PATH = "./model.h5"
 
@@ -55,11 +59,10 @@ app.layout = html.Div(
                 width=4,
             ),
             dbc.Col([
-                html.P(["Something"]),
+                html.P(["Project History"]),
                 html.Div([
                     dcc.Graph(
                         id='crossfilter-indicator-scatter',
-                        hoverData={'points': [{'customdata': 'Japan'}]}
                     )],
                 style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),],
             width=8),
@@ -75,15 +78,15 @@ def parse_contents(contents, filename, date):
     try:
         if "csv" in filename:
             # Assume that the user uploaded a CSV file
-            df = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
+            df = pd.read_csv(io.StringIO(decoded.decode("utf-8")),index_col=0)
         elif "xls" in filename:
             # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
+            df = pd.read_excel(io.BytesIO(decoded),index_col=0)
     except Exception as e:
         print(e)
         return html.Div(["There was an error processing this file."])
 
-    return html.Div(
+    return (df, html.Div(
         [
             html.H5(filename),
             html.H6(datetime.datetime.fromtimestamp(date)),
@@ -91,30 +94,51 @@ def parse_contents(contents, filename, date):
                 data=df.to_dict("records"),
                 columns=[{"name": i, "id": i} for i in df.columns],
             ),
-            html.Hr(),  # horizontal line
-            # For debugging, display the raw contents provided by the web browser
-            html.Div("Raw Content"),
-            html.Pre(
-                contents[0:200] + "...",
-                style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"},
-            ),
         ]
-    )
+    ))
 
 
 @app.callback(
     Output("output-data-upload", "children"),
+    Output('crossfilter-indicator-scatter', "figure"),
     Input("upload-data", "contents"),
     State("upload-data", "filename"),
     State("upload-data", "last_modified"),
 )
 def update_output(list_of_contents, list_of_names, list_of_dates):
+    children = None
+    data = None
+    fig = go.Figure(
+    )
     if list_of_contents is not None:
         children = [
             parse_contents(c, n, d)
             for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
         ]
-        return children
+        data, children = children[0]
+
+        if CSV_COLUMNS_AS_MONTHS:
+            print("transposing")
+            data = data.transpose()
+
+        data = data.dropna(how='all')
+
+        colors = {"graphBackground": "#F5F5F5", "background": "#ffffff", "text": "#000000"}
+        fig = go.Figure(
+            data=[
+                go.Scatter(
+                    x=data.index,
+                    y=data[col],
+                    mode='lines+markers') for col in data.columns
+                ],
+            layout=go.Layout(
+                plot_bgcolor=colors["graphBackground"],
+                paper_bgcolor=colors["graphBackground"]
+            )
+        )
+
+
+    return children, fig
 
 
 if __name__ == "__main__":
